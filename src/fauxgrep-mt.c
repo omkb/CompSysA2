2a25f48c21e28f9013f4fecee1ccddb080e42260
AlbertHytten <rxp491@alumni.ku.dk>
2024-10-13 14:42:59 +0200

    faugrep er implementeret og virker

--- src/fauxgrep-mt.c
+++ src/fauxgrep-mt.c
@@ -19,6 +19,56 @@
 
 #include "job_queue.h"
 
+// to pass the arguments to the worker
+struct worker_args {
+  struct job_queue *jq;
+  char const *needle;
+};
+
+int fauxgrep_file(char const *needle, char const *path) {
+  FILE *f = fopen(path, "r");
+
+  if (f == NULL) {
+    warn("failed to open %s", path);
+    return -1;
+  }
+
+  char *line = NULL;
+  size_t linelen = 0;
+  int lineno = 1;
+
+  while (getline(&line, &linelen, f) != -1) {
+    if (strstr(line, needle) != NULL) {
+      printf("%s:%d: %s \n", path, lineno, line);
+    }
+
+    lineno++;
+  }
+
+  free(line);
+  fclose(f);
+
+  return 0;
+}
+
+void *worker(void *arg) {
+  struct worker_args *args = arg;
+  struct job_queue *jq = args->jq;
+  char const *needle = args->needle;
+  char *file_path;
+
+  while (1) {
+    if (job_queue_pop(jq, (void**)&file_path) == 0) {
+      fauxgrep_file(needle, file_path);
+      free(file_path);
+    } else {
+      break;
+    }
+  }
+  return NULL;
+}
+
+
 int main(int argc, char * const *argv) {
   if (argc < 2) {
     err(1, "usage: [-n INT] STRING paths...");
@@ -51,7 +101,23 @@ int main(int argc, char * const *argv) {
     paths = &argv[2];
   }
 
-  assert(0); // Initialise the job queue and some worker threads here.
+  
+  // Create job queue
+  struct job_queue jq;
+  job_queue_init(&jq, 64);
+
+  // Create worker arguments
+  struct worker_args args;
+  args.jq = &jq;
+  args.needle = needle;
+
+  // Start up the worker threads
+  pthread_t *threads = calloc(num_threads, sizeof(pthread_t));
+  for (int i = 0; i < num_threads; i++) {
+    if (pthread_create(&threads[i], NULL, &worker, &args) != 0) {
+      err(1, "pthread_create() failed");
+    }
+  }
 
   // FTS_LOGICAL = follow symbolic links
   // FTS_NOCHDIR = do not change the working directory of the process
@@ -68,20 +134,36 @@ int main(int argc, char * const *argv) {
 
   FTSENT *p;
   while ((p = fts_read(ftsp)) != NULL) {
+    char *file_path = NULL; 
     switch (p->fts_info) {
     case FTS_D:
       break;
     case FTS_F:
-      assert(0); // Process the file p->fts_path, somehow.
+      // Duplicate the file path to push onto the job queue
+      file_path = strdup(p->fts_path);
+      if (file_path == NULL) {
+        err(1, "Failed to allocate memory for file path");
+      }
+      if (job_queue_push(&jq, file_path) != 0) {
+        free(file_path);
+        err(1, "Failed to push to job queue");
+        }
       break;
     default:
-      break;
+        break;
     }
-  }
+}
 
   fts_close(ftsp);
 
-  assert(0); // Shut down the job queue and the worker threads here.
+  job_queue_destroy(&jq);
+
+    for (int i = 0; i < num_threads; i++) {
+      if (pthread_join(threads[i], NULL) != 0) {
+      err(1, "pthread_join() failed");
+    }
+  }
+  free(threads);
 
   return 0;
 }
